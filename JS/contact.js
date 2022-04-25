02edf0e8bd9c3bebac1744b6aa32cd7b330e1e8e
DDaaNNee <50401918+DDaaNNee@users.noreply.github.com>
2022-04-25 10:59:05 +0200

    Attempt to add database via firebase

---
+++ JS/contact.js
@@ -0,0 +1,29 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCH0R7OPFAJIPNKFiOrbbbJXwwj6nunPjI",
+  authDomain: "project-team-red.firebaseapp.com",
+  projectId: "project-team-red",
+  storageBucket: "project-team-red.appspot.com",
+  messagingSenderId: "517903207132",
+  appId: "1:517903207132:web:2e4da9746ae2db94d34219",
+  measurementId: "G-2RJZC89REN"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var contactFormDB = firebase.database().ref('contact-form');
+
+document.getElementById('contact-form').addEventListener('submit', submitForm);
+
+function submitForm(e){
+  e.preventDefault();
+
+  var name = getElementVal('contact-name-input');
+  var email = getElementVal('contact-email-input');
+  var message = getElementVal('contact-message-input');
+
+  console.log(name, email, message);
+}
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+}
